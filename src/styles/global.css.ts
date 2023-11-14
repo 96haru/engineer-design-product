07e9aaeb345fdba2df425929c989e08118c0f45e
r-happy <hr325913@gmail.com>
2023-11-14 10:46:57 +0900

    setup css

---
+++ src/styles/global.css.ts
@@ -0,0 +1,46 @@
+import { globalStyle } from "@vanilla-extract/css";
+import { Colors } from "./colors";
+import { mediaQueryScreen } from "./queries";
+
+globalStyle("body", {
+    fontFamily: "var(--font-roboto)",
+    backgroundColor: Colors.background.body.light,
+    color: Colors.color.main_text.light,
+});
+
+globalStyle("p, button", {
+    fontSize: "small",
+    fontWeight: "normal",
+
+    "@media": {
+        [mediaQueryScreen("md")]: {
+            fontSize: "medium",
+        },
+    },
+});
+
+globalStyle("h2", {
+    fontSize: "large",
+    fontWeight: "bold",
+
+    "@media": {
+        [mediaQueryScreen("md")]: {
+            fontSize: "x-large",
+        },
+    },
+});
+
+globalStyle("h1", {
+    fontSize: "x-large",
+    fontWeight: "bolder",
+
+    "@media": {
+        [mediaQueryScreen("md")]: {
+            fontSize: "xx-large",
+        },
+    },
+});
+
+globalStyle("body button", {
+    cursor: "pointer",
+});
