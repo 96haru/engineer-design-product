93bf543acfe629406e23aa103db5fd0f3ea9cd78
r-happy <hr325913@gmail.com>
2023-11-14 12:54:22 +0900

    add hero

--- src/app/page.css.ts
+++ src/app/page.css.ts
@@ -1,9 +1,34 @@
+import { Colors } from "@/styles/colors";
+import { mediaQueryScreen } from "@/styles/queries";
 import { style } from "@vanilla-extract/css";
 
 export const hero_wrapper = style({
-    height: "100dvh",
-    width: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
-})
+  height: "80dvh",
+  minHeight: "720px",
+  width: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  position: "relative",
+  background: Colors.background.hero.light,
+});
+
+export const hero_content = style({});
+
+export const hero_title = style({
+  fontSize: "3rem",
+  fontFamily: "var(--font-space_mono)",
+
+  "@media": {
+    [mediaQueryScreen("md")]: {
+      fontSize: "5rem",
+    },
+  },
+});
+
+export const hero_wave = style({
+  width: "100%",
+  position: "absolute",
+  bottom: "0px",
+  left: "0px",
+});
